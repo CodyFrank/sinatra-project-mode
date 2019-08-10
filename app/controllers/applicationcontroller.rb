fa78c71ccfc1730a3683e7e41ef71904c2a85c89
Cody <cody.frank30@gmail.com>
2019-08-10 19:46:52 -0400

    set appcontroller configure block -- no session secret set yet

--- app/controllers/applicationcontroller.rb
+++ app/controllers/applicationcontroller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < Sinatra::Base
 
+
+    configure do
+        set :views, 'app/views'
+        set :public_folder, 'public'
+        enable :sessions
+    end
+
     get '/' do
         "Hello World"
     end
