1a902a23425bee2bc44ca79a3f88889b1a2e7884
Cody <cody.frank30@gmail.com>
2019-08-17 17:50:47 -0400

    started all pokemon routes that will be in project

--- app/controllers/pokemoncontroller.rb
+++ app/controllers/pokemoncontroller.rb
@@ -1,2 +1,27 @@
 class PokemonController < ApplicationController
+
+  get '/pokemon' do
+  end
+
+  get '/pokemon/new' do
+  end
+
+  post '/pokemon' do
+  end
+
+  get '/pokemon/:id' do
+  end
+
+  get '/pokemon/:id/edit' do
+  end
+
+  patch '/articles/:id' do
+  end
+
+  put '/articles/:id' do
+  end
+
+  delete '/articles/:id' do
+  end
+
 end
