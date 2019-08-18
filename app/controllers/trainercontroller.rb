a0da756aa37b15cb17e7db3cde4e6f5096a029cb
Cody <cody.frank30@gmail.com>
2019-08-18 17:21:54 -0400

    added error page to other routes

--- app/controllers/trainercontroller.rb
+++ app/controllers/trainercontroller.rb
@@ -7,43 +7,46 @@ class TrainerController < ApplicationController
   
   get '/trainers/:id' do
     authenticate
-    @trainer = Trainer.find_by_id(params[:id])
-    erb :'/trainers/show'
+    if @trainer = Trainer.find_by_id(params[:id])
+      erb :'/trainers/show'
+    else
+      erb :'error'
+    end
   end
 
   get '/trainers/:id/edit' do
     authenticate
-    @trainer = Trainer.find_by_id(params[:id])
-    if @trainer == current_user
-      erb :'trainers/edit'
+    if @trainer = Trainer.find_by_id(params[:id])
+      if @trainer == current_user
+        erb :'trainers/edit'
+      else
+        redirect "/trainers/#{@trainer.id}"
+      end
     else
-      redirect "/trainers/#{@trainer.id}"
+      erb :'error'
     end
   end
 
-  patch '/trainers/:id' do
-    authenticate
-    @trainer = Trainer.find_by_id(params[:id])
-    if @trainer.id == current_user.id
+  # patch '/trainers/:id' do
+  #   authenticate
+  #   @trainer = Trainer.find_by_id(params[:id])
+  #   if @trainer.id == current_user.id
 
-      if params[:trainer_name] != ""
-        @trainer.trainer_name = params[:trainer_name]
-      end
+  #     if params[:trainer_name] != ""
+  #       @trainer.trainer_name = params[:trainer_name]
+  #     end
 
-      if params[:email] != ""
-        @trainer.email = params[:email]
-      end
-
-      puts "#{@trainer.email} email"
-      puts "#{@trainer.trainer_name} Trainer_name"
+  #     if params[:email] != ""
+  #       @trainer.email = params[:email]
+  #     end
       
-        @trainer.save
-        redirect "/trainers/#{@trainer.id}/edit"
+  #       @trainer.save
+  #       redirect "/trainers/#{@trainer.id}/edit"
       
 
-    else
-      redirect "/trainers/#{@trainer.id}"
-    end
-  end
+  #   else
+  #     redirect "/trainers/#{@trainer.id}"
+  #   end
+  # end
 
 end
