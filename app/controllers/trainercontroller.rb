23200352a6597cd76cbc0056206fbab7049a63c3
Cody <cody.frank30@gmail.com>
2019-08-17 21:26:38 -0400

    created trainer/id route

--- app/controllers/trainercontroller.rb
+++ app/controllers/trainercontroller.rb
@@ -54,5 +54,11 @@ class TrainerController < ApplicationController
     authenticate
     erb :'trainers/index'
   end
+  
+  get '/trainers/:id' do
+    authenticate
+    @trainer = Trainer.find_by_id(params[:id])
+    erb :'/trainers/show'
+  end
 
 end
