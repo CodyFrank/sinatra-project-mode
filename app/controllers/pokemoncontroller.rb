c687b9eb96a5a8bf03732b6e2c49d12ffcd4c7aa
Cody <cody.frank30@gmail.com>
2019-08-19 18:54:32 -0400

    added error page into pokemon route to handle bad urls

--- app/controllers/pokemoncontroller.rb
+++ app/controllers/pokemoncontroller.rb
@@ -28,57 +28,72 @@ class PokemonController < ApplicationController
     if @pokemon = Pokemon.find_by_id(params[:id])
       erb :'pokemon/show'
     else
+      @messages = "oops! looks like that pokemon doesn't exist please try again."
       erb :'error'
     end
   end
 
   get '/pokemon/:id/edit' do
     authenticate
-    @pokemon = Pokemon.find_by_id(params[:id])
-    if @pokemon.trainer.id == current_user.id
-        erb :'pokemon/edit'
+    if @pokemon = Pokemon.find_by_id(params[:id])
+      if @pokemon.trainer.id == current_user.id
+          erb :'pokemon/edit'
+      else
+          @messages = "That is not your Pokemon to edit"
+          erb :error
+      end
     else
-        redirect '/pokemon'
+      @messages = "oops! looks like that pokemon doesn't exist please try again."
+      erb :error
     end
   end
 
   patch '/pokemon/:id' do
     authenticate
-    @pokemon = Pokemon.find_by_id(params[:id])
-    if @pokemon.trainer.id == current_user.id
-
-        if params[:name] != ""
-          @pokemon.name = params[:name]
-        end
-
-        if params[:nickname] != ""
-          @pokemon.nickname = params[:nickname]
-        end
-
-        if params[:element] != ""
-          @pokemon.element = params[:element]
-        end
-
-        if @pokemon.valid?
-          @pokemon.save
-          redirect "pokemon/#{@pokemon.id}"
-        else
-          @messages = "Something has gone wrong"
-          erb :'pokemon/edit'
-        end
-    else
-        redirect "pokemon/#{@pokemon.id}/edit"
+    if @pokemon = Pokemon.find_by_id(params[:id])
+      if @pokemon.trainer.id == current_user.id
+  
+          if params[:name] != ""
+            @pokemon.name = params[:name]
+          end
+  
+          if params[:nickname] != ""
+            @pokemon.nickname = params[:nickname]
+          end
+  
+          if params[:element] != ""
+            @pokemon.element = params[:element]
+          end
+  
+          if @pokemon.valid?
+            @pokemon.save
+            redirect "pokemon/#{@pokemon.id}"
+          else
+            @messages = "Something has gone wrong"
+            erb :'pokemon/edit'
+          end
+      else
+          @messages = "That is not your Pokemon to edit"
+          erb :error
+      end
+    else 
+      @messages = "oops! looks like that pokemon doesn't exist please try again."
+      erb :error
     end
   end
 
   delete "/pokemon/:id" do
     authenticate
-    @pokemon = Pokemon.find_by_id(params[:id])
-    if @pokemon.trainer.id == current_user.id
-        @pokemon.delete
-        redirect '/pokemon'
-    else 
-        redirect "/pokemon/#{@pokemon.id}"
+    if @pokemon = Pokemon.find_by_id(params[:id])
+      if @pokemon.trainer.id == current_user.id
+          @pokemon.delete
+          redirect '/pokemon'
+      else 
+          redirect "/pokemon/#{@pokemon.id}"
+      end
+    else
+      @messages - "oops! looks like that pokemon doesn't exist please try again."
+      erb :error
     end
   end
 
